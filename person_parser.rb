31dc3fdcfbd42d55091c9aa99c5ec13e3e769408
sergener <sibel.ergener@gmail.com>
2016-08-25 11:46:13 -0400

    method to parse every file

--- person_parser.rb
+++ person_parser.rb
@@ -22,36 +22,44 @@ class PersonParser
 	# people_array
  #  end
 
+
   def file_names
   	# @files = Dir.entries("input-files").select {|f| !File.directory? f}
   	@files = Dir.glob("input-files/*")
-  	binding.pry
   end
 
   def convert_headers(string)
   	return string.gsub(/(.)([A-Z])/,'\1_\2').downcase.to_sym
   end
 
-  def standardize_separater
-  	@person_array = []
-  	File.readlines(@file).each do |line|
-  	  @person_array << line.gsub(/[,| ]/, ' ').split(" ")
+  def standardize_separater(file)
+  	person_array = []
+  	File.readlines(file).each do |line|
+  	  person_array << line.gsub(/[,| ]/, ' ').split(" ")
+  	end
+  	person_array
+  end
+
+  def create_people_objects(file)
+  	person_array = self.standardize_separater(file)
+  	headers = person_array.shift
+  	headers.map! {|header| convert_headers(header)}
+  	person_array.each do |person|
+  		@people << Person.new(headers.zip(person).to_h)
   	end
   end
 
-  def create_people_objects
-  	headers = @person_array.shift
-  	headers.map! {|header| convert_headers(header)}
-  	binding.pry
-  	@person_array.each do |person|
-  		@people << Person.new(headers.zip(person).to_h)
+  def parse_every_file
+  	self.file_names
+  	@files.each do |file|
+  		self.create_people_objects(file)
   	end
-  	@people
+  	binding.pry
   end
 
 end
-people = PersonParser.new('input-files/file2.txt')
+people = PersonParser.new
 # people.parse_people_objects_from_array
 # people.file_to_arrays
 # people.create_hash
-people.file_names
+people.parse_every_file
