5ba4cf8d50229875f4a44b2ab2270c401db6e1e3
sergener <sibel.ergener@gmail.com>
2016-08-26 11:48:46 -0400

    Fixed tests

--- spec/person_parser_spec.rb
+++ spec/person_parser_spec.rb
@@ -3,13 +3,22 @@ require_relative '../person_parser'
 describe PersonParser do
 
   let(:parser) { PersonParser.new }
+  let(:files) {["input-files/file1.txt", "input-files/file2.txt", "input-files/file3.txt"]}
 
   	it "should initialize an array of people objects" do
   		expect(parser.people).to all(be_a(Person))
   	end
 
-	it "should initialize people from every file" do
-		expect(parser.people.length).to eq(9)
+	it "should create array of all file names from input-files" do
+	    expect(parser.send(:file_names)).to eq ["input-files/file1.txt", "input-files/file2.txt", "input-files/file3.txt"]
+	end
+
+	it "should create all people objects from all files" do
+  		lines_in_files = files.map do |file|
+  			File.foreach(file).inject(0) {|sum, line| sum+1}
+  		end
+  		people_count = (lines_in_files.inject(:+)) - (parser.send(:file_names).length)
+  		expect(people_count).to eq(9)
 	end
 
 end
